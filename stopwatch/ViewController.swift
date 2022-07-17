9619d0d2512d5514003afb1be4082cc7bee0a879
maximpolovinkin <polloVinkin@yandex.ru>
2022-07-17 16:02:47 +0300

    begin

--- stopwatch/ViewController.swift
+++ stopwatch/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 50
     }
 
 
